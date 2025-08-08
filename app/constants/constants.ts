8628742335064ec084bea13b7b1ad73b5c3c8d1a
Adrián Avilés <92331312+AdrianAviles@users.noreply.github.com>
2025-08-08 09:15:21 -0600

    añadido partner_companies en cookies login

--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -20,6 +20,7 @@ export default {
     ROUTE_GET_DPS_BY_AREA_ID: '/transactions/documents/list-doc-by-functional-area/',
     ROUTE_DOWNLOAD_FILES_DPS: '/transactions/documents/download-zip/',
     ROUTE_GET_DPS_LIMIT_DATES: '/transactions/upload-deadline/check-deadline/',
+    ROUTE_GET_COMPANIES: '/transactions/partners/list-companies/',
 
     //Constantes de estados de las facturas
     REVIEW_ACCEPT: 'A',
